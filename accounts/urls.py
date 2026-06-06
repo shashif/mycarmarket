777e1b6bce6e982e377a373de6bf2c0769f3c786
Md Shoriful Hasan <ashif@Mds-MacBook-Air.local>
2026-06-06 18:15:31 +1000

    v0.4.8 verification completed with login logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,29 @@
 # ==========================================
 # MyCarMarket
-# Version: v0.4.0
+# Version: v0.4.8
 # File: accounts/urls.py
+# Login + Logout Added
 # ==========================================
 
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+
+    path(
+        'login/',
+        LoginView.as_view(
+            template_name='registration/login.html'
+        ),
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        LogoutView.as_view(),
+        name='logout'
+    ),
 ]
