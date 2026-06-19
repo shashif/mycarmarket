98c9771fd5c66dfef2716efb95d9394cd5b4ca3d
Md Shoriful Hasan <ashif@Mds-MacBook-Air.local>
2026-06-19 10:29:53 +1000

    v1.3.1 UX improvements homepage loader toast scroll button

---
+++ static/js/loading.js
@@ -0,0 +1,15 @@
+function showLoader() {
+
+    document.getElementById(
+        'global-loader'
+    ).style.display = 'flex';
+
+}
+
+function hideLoader() {
+
+    document.getElementById(
+        'global-loader'
+    ).style.display = 'none';
+
+}
---
+++ static/js/toast.js
@@ -0,0 +1,17 @@
+function showToast(message) {
+
+    const toast = document.getElementById(
+        'toast'
+    );
+
+    toast.innerText = message;
+
+    toast.style.display = 'block';
+
+    setTimeout(() => {
+
+        toast.style.display = 'none';
+
+    }, 3000);
+
+}
